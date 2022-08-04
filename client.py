711328db2a121160cea1ca9f6c50cf7d01f08ac2
James Bush <jamesfbush@protonmail.com>
2022-08-04 15:42:47 -0700

    more fit and finish / refactor

--- client.py
+++ client.py
@@ -1,3 +1,8 @@
+# Author:        James Bush
+# Date:          2022-08-04
+# Description:   Implementation of server-client chat per instructions.
+# Sources:       https://docs.python.org/3/howto/sockets.html
+
 import socket
 import sys
 import server 
@@ -7,68 +12,30 @@ import server
 class MyClient(server.MySocket):
     def __init__(self, role='client', host='localhost',port=12000):
         super().__init__(role, host, port)
+ 
      
-
-    # *****************OLD MyCient********************
-    # def __init__(self, sock=None):
-    #     if sock is None:
-    #         self.sock = socket.socket(
-    #                         socket.AF_INET, socket.SOCK_STREAM)
-    #     else:
-    #         self.sock = sock
-    #     self.MSGLEN = 4 # set msglength to 4, the length of the header
-
-    # # Method set up connection with host        
-    # def connect(self, host, port) :
-    #     self.sock.connect((host, port))
-
-    # # Method sends message and shows error if connect broke
-    # def mysend(self, msg):
-    #     totalsent = 0
-    #     length = str((len(msg) + 4)).zfill(4) # pads with leading 0's
-    #     msg = length + msg
-    #     while totalsent < len(msg):
-    #         sent = self.sock.send(msg[totalsent:].encode())
-    #         if sent == 0:
-    #             raise RuntimeError("socket connection broken")
-    #         totalsent = totalsent + sent
-
-    # # Method receives message, checks the length to receive correct amount
-    # def myreceive(self):
-    #     chunks = []
-    #     newchunks = []
-    #     bytes_recd = 0
-    #     while bytes_recd < self.MSGLEN:
-    #         chunk = self.sock.recv(min(self.MSGLEN - bytes_recd, 2048))
-    #         if chunk == b'': # if received 0 bytes then connection broken
-    #             raise RuntimeError("socket connection broken")
-    #         chunks.append(chunk)
-    #         bytes_recd = bytes_recd + len(chunk)
-    #     length = int(b''.join(chunks))
-    #     while bytes_recd < length:
-    #         newchunk = self.sock.recv(min(length - bytes_recd, 2048))
-    #         if newchunk == b'':
-    #             raise RuntimeError("socket connection broken")
-    #         newchunks.append(newchunk)
-    #         bytes_recd = bytes_recd + len(newchunk)
-    #     return b''.join(newchunks)
-    # *****************OLD MyCient********************
+if __name__ == "__main__":
 
 
+    # Start chat connection, set role 
+    chatConnection = server.MySocket('client')
+    correspondent = chatConnection.getCorrespondent()
+    # Main chat loop 
+    while True:
 
-# Begin Constructor 
+        # Enter message to send / send it 
+        msgSend = input("> ")
+        chatConnection.sendMsg(msgSend)
 
-# Starts the connection on the following host and port
-# clientConnection = MyClient()
-chatConnection = server.MySocket('client')
-# clientConnection.connect('localhost', 12000)
-print("Press any key to get started and connect to server")
-while True:
-    msgSend = input("> ")
-    chatConnection.mysend(msgSend)
-    if msgSend == '/q': # close the connection
-        print("Exiting...")
-        chatConnection.sock.close()
-        break
-    msgRec = chatConnection.myreceive()#.decode()
-    print(msgRec)    
+        # If the message is /q to quit, close the socket and exit 
+        if msgSend == '/q': 
+            print("Exiting...")
+            chatConnection.sock.close()
+            break
+    
+        # Otherwise, receive messages from server and print to console
+        msgRec = chatConnection.receiveMsg()
+
+        #print(f"{correspondent}: {msgRec}")    
+
+        print(f"\n{correspondent}: {msgRec}\n")
--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+# Author:        James Bush
+# Date:          2022-08-04
+# Description:   Implementation of server-client chat per instructions.
+# Sources:       https://docs.python.org/3/howto/sockets.html
 
 from socket import *
 import random
@@ -5,70 +9,77 @@ import random
 class MySocket:
 
     def __init__(self, role=None, host='localhost',port=12000):
+
         self.MSGLEN = 4
+
         if role == 'server':
             self.role = role
             self.sock = socket(AF_INET, SOCK_STREAM)
-            self.sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-            ## TRYING THIS
-            self.sock.bind((host, port)) # was '' in place of host
-            # set to listen for TCP connections
-            self.sock.listen(1)
-            # set connection
+            self.sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1) # reuse port
+            self.sock.bind((host, port))
+            self.sock.listen(1) # 1? 5? 
             self.clientConnection, self.addr = self.sock.accept()
-            print("Chat running...")            
-            ## -----------
+            print("Connected to chat!")            
+
         elif role == 'client':
             self.role = role
             self.sock = socket(AF_INET, SOCK_STREAM)
             self.sock.connect((host, port))
             self.clientConnection = self.sock # the connection is itself 
 
-    # TODO refactor 
-    # Method sends message and adds a header indicating length of msg
-    def mysend(self, msg):
+    def sendMsg(self, msg):
+        """Send string with header indicating length of string.
+        """
         totalsent = 0
-        length = str((len(msg) + 4)).zfill(4) # pads with leading 0's
-        msg = length + msg
+        msg = f"{str((len(msg) + 4)).zfill(4)}{msg}"
         while totalsent < len(msg):
             sent = self.clientConnection.send(msg[totalsent:].encode())
             if sent == 0:
                 raise RuntimeError("socket connection broken")
-            totalsent = totalsent + sent
+            totalsent += sent
 
-    # TODO refactor
-    # Method receives message, checks the length to receive correct amount
-    def myreceive(self):
+    def receiveMsg(self):
+        """Receive string message by chunks. Error if connection broke. 
+        """
         chunks = []
-        newchunks = []
-        bytes_recd = 0
-        while bytes_recd < self.MSGLEN:
-            chunk = self.clientConnection.recv(min(self.MSGLEN - bytes_recd, 2048))
-            if chunk == b'': # if received 0 bytes then connection broken
+        newChunks = []
+        bytesRecd = 0
+        while bytesRecd < self.MSGLEN:
+            chunk = self.clientConnection.recv(min(self.MSGLEN - bytesRecd, 2048))
+            # connection broken
+            if chunk == b'': 
                 raise RuntimeError("socket connection broken")
             chunks.append(chunk)
-            bytes_recd = bytes_recd + len(chunk)
+            bytesRecd += len(chunk)
         length = int(b''.join(chunks))
-        while bytes_recd < length:
-            newchunk = self.clientConnection.recv(min(length - bytes_recd, 2048))
+        while bytesRecd < length:
+            newchunk = self.clientConnection.recv(min(length - bytesRecd, 2048))
+            # connection broken
             if newchunk == b'':
                 raise RuntimeError("socket connection broken")
-            newchunks.append(newchunk)
-            bytes_recd = bytes_recd + len(newchunk)
-        #correspondent = [i for i in ['server','client'] if i != self.role][0].capitalize()
-        # message = self.strColor(f"{b''.join(newchunks).decode()}\n", 'blue')
+            newChunks.append(newchunk)
+            bytesRecd += len(newchunk)
+
         # return message
-        return b''.join(newchunks).decode()
+        return b''.join(newChunks).decode()
+
+    def getCorrespondent(self):
+        """Return correspondent entity name."""
+        return [i for i in ['server','client'] if i != self.role][0].capitalize()
 
     def strColor(self,string,color):
+        """Take string, make it colorful."""
         colors = {  
             'blue':str(34),
-            'red':str(91)
+            'red':str(91),
+            'green':str(92),
+            'cyan':str(96)
         }
         return f"\033[{colors[color]}m{string}\033[00m"
 
     def networkingTriviaQA(self):
-        
+        """When called, return random question and possible answers from bank.
+        """
         questionAnsBank = [ 
             ['TCP is a connection-oriented service [T/F]\n',['true','t']],
             ['UDP is a \"fire and forget\" type of service [T/F]:\n',['true','t']],
@@ -77,62 +88,83 @@ class MySocket:
         return questionAnsBank[random.randint(0, len(questionAnsBank)-1)]
 
     def networkingTrivia(self):
-        welcome = f"{'*'*30}\nWelcome to networking trivia"
+        """ Ask questions from bank, sending question through chat. 
+        Take answers through chat, check against model answers, provide feedback. 
+        When receive /stop, stop game and send score. 
+        """
+        welcome = self.strColor(f"\n{'*'*40}\nWelcome to Network Trivia!\n{'*'*40}\n",'cyan')
         score = 0
         questions = 0 
-        # qAndA = {   'TCP is a connection-oriented service [T/F]':['true','t'],
-        #             'UDP is a \"fire and forget\" type of service [T/F]:':['true','t']}
-        
-        qAndA = self.networkingTriviaQA()
-        # need support or asked questions
 
+        # main play loop
         while True:
             qAndA = self.networkingTriviaQA()
             question = qAndA[0]
             answers = qAndA[1]
-            print(question) # send message
-            self.mysend(f"{welcome}\n\n{question}")
-            #answer = input("Answer: ").lower() # take response 
-            answer = self.myreceive()
-            print("PARSED ANS",len(answer),answer)
-            if answer == "\stop":
+            print(question) # see question server-side
+
+            # welcome greeting / ask question 
+            if questions == 0: 
+                self.sendMsg(f"{welcome}\nQuestion {questions+1}: {question}")
+            else: 
+                self.sendMsg(f"\nQuestion {questions+1}: {question}")
+
+            # obtain and parse answer 
+            answer = self.receiveMsg().lower()
+
+            # stop if requested 
+            if answer == "/stop":
                 break
+
+            # check substantive answer, generate appropriate feedback 
             elif answer in answers:
                 score += 1
                 questions += 1
-                print("Correct!") # send message
-                self.mysend("Correct")
+                feedback = self.strColor('Correct!', 'green')
             else:
                 questions += 1
-                message = f"incorrect. acceptable answers are {[str(i) for i in answers]}"
-                print(self.strColor(message,'red')) # send message
-                self.mysend(message)
-        message = f"Score: {(score/questions)*100}%"
-        print(message) # send message 
-        self.mysend(message)
+                feedback = self.strColor(f"Incorrect. Acceptable answers are {[str(i) for i in answers]}",'red')
+
+            # send feedback on answer
+            message = f"{feedback}\n\nHit Enter/Return for next question or '/stop' to quit and see score.\n"
+            self.sendMsg(message)
+
+            # get response, whether to continue or get score and quit
+            response = self.receiveMsg().lower()
+            print(response)
+            if response == "/stop":
+                break
+            
+        # send score and return to main program 
+        if questions > 0:
+            score = round((score/questions),3)*100
+        elif questions == 0:
+            score = 0
+        message = f"\n{'*'*40}\nScore: {score}%\nThanks for playing Network Trivia!\n{'*'*40}\n"
+        print(message) 
+        self.sendMsg(message)
+        return
 
 if __name__ == "__main__":
     # Initiate chat session
     chatConnection = MySocket('server')
+    correspondent = chatConnection.getCorrespondent()
 
-
-    # Loop will permit basic back-and-forth chat
+    # Loop will permit basic, turn-oriented chat
     while True:
-            correspondent = [i for i in ['server','client'] if i != chatConnection.role][0].capitalize()
-            message = chatConnection.myreceive()#.decode()
-            # if (message == '/q'): #close connection if /q
-            if '/q' in message:
+            #correspondent = [i for i in ['server','client'] if i != chatConnection.role][0].capitalize()
+            msgRec = chatConnection.receiveMsg()#.decode()
+            # close connection if /q in message 
+            if '/q' in msgRec:
                 print("Exiting...")
                 chatConnection.sock.close()
                 break
-                # needs to quit the session/close down whatever
-            if '\game' in message:
+            # EXTRA CREDIT - start game if /game in message
+            if '/game' in msgRec:
                 chatConnection.networkingTrivia()
-            print(f"\n{correspondent}: {message}")
-            reply = input("> ")
-            chatConnection.mysend(reply)
-            # if (message == 'quit'): #close connection if /q
-            #     break
-            #clientConnection.mysend(result) 
-            
+            else:
+                print(f"\n{correspondent}: {msgRec}\n")
+                reply = input("> ")
+                chatConnection.sendMsg(reply)
+
 
